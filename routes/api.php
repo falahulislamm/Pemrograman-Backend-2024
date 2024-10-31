45ffbb287f70d0ea1f2fa12b4bb544314473f18f
falahulislamm <fal.software512@gmail.com>
2024-10-31 07:22:20 +0700

    Pertemuan 5 - RESTful API

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\StudentController;
 
+// ANIMAL TUGAS PERT 4
 Route::get('/animals', [AnimalController::class, 'index']);
 Route::post('/animals', [AnimalController::class, 'store']);
 Route::put('/animals/{id}', [AnimalController::class, 'update']);
 Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
+
+// STUDENTS TUGAS PERT 5
+Route::get('/students', [StudentController::class, 'index']);
+Route::post('/students', [StudentController::class, 'store']);
+Route::put('/students/{id}', [StudentController::class, 'update']);
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
